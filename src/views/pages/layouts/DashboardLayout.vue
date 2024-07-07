671b73c9b0fd631c68a4f4e11cd88303f904cc90
Erick Sousa <23417131+ericksousa@users.noreply.github.com>
2024-07-06 23:31:43 -0300

    feat: cria componente do header do dashboard

--- src/views/pages/layouts/DashboardLayout.vue
+++ src/views/pages/layouts/DashboardLayout.vue
@@ -1,4 +1,12 @@
+<script lang="ts" setup>
+import HeaderComponent from "@/views/components/header/HeaderComponent.vue"
+</script>
+
 <template>
-    <h1>Autenticado</h1>
-    <router-view />
+    <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <HeaderComponent />
+            <router-view />
+        </div>
+    </main>
 </template>
